a8ac4348f23c74299d20c7f74177377887226882
Denise Bischof <denisebischof@web.de>
2018-04-19 22:01:22 +0100

    upload

---
+++ Assets/UnityServer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+
+
+public class UnityServer : MonoBehaviour {
+
+    void OnGUI()
+    {
+        string ipaddress = Network.player.ipAddress;
+        GUI.Box(new Rect(10, Screen.height - 50, 100, 50), ipaddress);
+        GUI.Label(new Rect(20, Screen.height - 35, 100, 20), "Status:" + NetworkServer.active);
+        GUI.Label(new Rect(20, Screen.height - 20, 100, 20), "Connected:" + NetworkServer.connections.Count);
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        NetworkServer.Listen(25000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
